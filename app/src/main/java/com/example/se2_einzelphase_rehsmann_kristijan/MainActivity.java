cfc6d67f2f3ba42a7989cea5a68695cd26aed7aa
squishyjoski <kris.rehsmann@gmail.com>
2021-03-11 20:02:48 +0100

    programmierung und testen von Eingabefeld und Button

--- app/src/main/java/com/example/se2_einzelphase_rehsmann_kristijan/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase_rehsmann_kristijan/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.se2_einzelphase_rehsmann_kristijan;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         abschickenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText EingabeMatrikelnummer = (EditText) findViewById(R.id.EingabeMatrikelnummerEditTextNumber);
+                TextView Antwort = (TextView) findViewById(R.id.AntwortTextView);
+
+                int Matrikelnummer = Integer.parseInt(EingabeMatrikelnummer.getText().toString());
+                int result = Matrikelnummer + 1;
+                Antwort.setText(result + "");
 
             }
         });
