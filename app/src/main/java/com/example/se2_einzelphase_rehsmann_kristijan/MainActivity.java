0bea886d7f9b4f41a7cf60ed32ac182bd7ab488b
squishyjoski <kris.rehsmann@gmail.com>
2021-03-11 19:50:25 +0100

    Graphische Oberfläche gebaut

--- app/src/main/java/com/example/se2_einzelphase_rehsmann_kristijan/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelphase_rehsmann_kristijan/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.se2_einzelphase_rehsmann_kristijan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button abschickenButton = (Button) findViewById(R.id.abschickenButton);
+        abschickenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
